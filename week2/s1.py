9e711c114690528b780f14cb3fb58ac82ca57bb8
Mario Nolasco Cortez <mariocortez7745@gmail.com>
2026-03-03 19:08:27 -0600

    Completed week 2, session 1, problem 7

--- week2/s1.py
+++ week2/s1.py
@@ -45,10 +45,16 @@ def max_audience_performances(audiences):
    
    return maxVal * dic[maxVal]
 
+def max_audience_performances2(audiences):
+   maxVal = max(audiences)
+   count = audiences.count(maxVal)
+
+   return maxVal * count
+
 audiences1 = [100, 200, 200, 150, 100, 250]
 audiences2 = [120, 180, 220, 150, 220]
 
-print(max_audience_performances(audiences1))
-print(max_audience_performances(audiences2))
+print(max_audience_performances2(audiences1))
+print(max_audience_performances2(audiences2))
 
 
