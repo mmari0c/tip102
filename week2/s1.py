4d8db8257af3f8b21c7b39e86c9e55d53b2455bd
Mario Nolasco Cortez <mariocortez7745@gmail.com>
2026-03-03 19:39:12 -0600

    Completed week 2, session 1, problem 9

--- week2/s1.py
+++ week2/s1.py
@@ -85,12 +85,4 @@ def num_popular_pairs(popularity_scores):
    
 
 
-popularity_scores1 = [1, 2, 3, 1, 1, 3]
-popularity_scores2 = [1, 1, 1, 1]
-popularity_scores3 = [1, 2, 3]
-
-print(num_popular_pairs(popularity_scores1))
-print(num_popular_pairs(popularity_scores2))
-print(num_popular_pairs(popularity_scores3))
-
 
