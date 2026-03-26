8380b67cc5c40fcfaf232c9c802451c41e34eb50
Mario Nolasco Cortez <mariocortez7745@gmail.com>
2026-03-26 12:06:23 -0500

    Completed w5, s2, p3

--- week5/s2.py
+++ week5/s2.py
@@ -44,27 +44,73 @@ class Villager:
 
 # 2
 
-class Node:
-   def __init__(self, value, next=None):
-      self.value = value
-      self.next = next
+# class Node:
+#    def __init__(self, value, next=None):
+#       self.value = value
+#       self.next = next
 
-# For testing
-def print_linked_list(head):
-   current = head
-   while current:
-      print(current.value, end=" -> " if current.next else "\n")
-      current = current.next
+# # For testing
+# def print_linked_list(head):
+#    current = head
+#    while current:
+#       print(current.value, end=" -> " if current.next else "\n")
+#       current = current.next
 
-kk_slider = Node("K.K. Slider")
-harriet = Node("Harriet")
-saharah = Node("Saharah")
-isabelle = Node("Isabelle")
+# kk_slider = Node("K.K. Slider")
+# harriet = Node("Harriet")
+# saharah = Node("Saharah")
+# isabelle = Node("Isabelle")
 
-kk_slider.next = harriet
-harriet.next = saharah
-saharah.next = isabelle
+# kk_slider.next = harriet
+# harriet.next = saharah
+# saharah.next = isabelle
 
-print_linked_list(kk_slider)
+# print_linked_list(kk_slider)
 
-# Add code here to link the above nodes
+# 3
+
+# class Node:
+#     def __init__(self, value, next=None):
+#         self.value = value
+#         self.next = next
+
+# # For testing
+# def print_linked_list(head):
+#     current = head
+#     while current:
+#         print(current.value, end=" -> " if current.next else "\n")
+#         current = current.next
+
+# def add_first(head, task):
+#    pass
+   # input: node instance -> head of a linked list, string -> new task we want to add to the front of linked list
+   # output: node instance -> new head of linked list
+
+   # understand: Create a new node with it's value being the task passed to the function, we then want to append that new node to the front of linked list and return the new node, now being the head
+
+   # plan:
+
+   # create a new instance of Node with a value = task
+   # append that new Node at the front of list
+      # new node points to the curr head
+   # return new Node
+
+#    new_task = Node(task)
+#    new_task.next = head
+
+#    return new_task
+
+# task_1 = Node("shake tree")
+# task_2 = Node("dig fossils")
+# task_3 = Node("catch bugs")
+# task_1.next = task_2
+# task_2.next = task_3
+
+# # Linked List: shake tree -> dig fossils -> catch bugs
+# print_linked_list(add_first(task_1, "check turnip prices"))
+
+
+
+
+
+
