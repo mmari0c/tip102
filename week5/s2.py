920d039a0061f3db855eb4dd7f266d085de61fbb
Mario Nolasco Cortez <mariocortez7745@gmail.com>
2026-03-26 11:58:11 -0500

    Completed w5, s2, p2

--- week5/s2.py
+++ week5/s2.py
@@ -41,3 +41,30 @@ class Villager:
 
 # ankha.friends = [marshal]
 # print(bob.get_mutuals(ankha))
+
+# 2
+
+class Node:
+   def __init__(self, value, next=None):
+      self.value = value
+      self.next = next
+
+# For testing
+def print_linked_list(head):
+   current = head
+   while current:
+      print(current.value, end=" -> " if current.next else "\n")
+      current = current.next
+
+kk_slider = Node("K.K. Slider")
+harriet = Node("Harriet")
+saharah = Node("Saharah")
+isabelle = Node("Isabelle")
+
+kk_slider.next = harriet
+harriet.next = saharah
+saharah.next = isabelle
+
+print_linked_list(kk_slider)
+
+# Add code here to link the above nodes
