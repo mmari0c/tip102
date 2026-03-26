ccee534b200f43fcd8c6c6bad80c2c8811841edf
Mario Nolasco Cortez <mariocortez7745@gmail.com>
2026-03-26 13:08:06 -0500

    Completed w5, s2, p6

--- week5/s2.py
+++ week5/s2.py
@@ -155,6 +155,49 @@ class Villager:
 
 # 5
 
+# class Node:
+#     def __init__(self, value, next=None):
+#         self.value = value
+#         self.next = next
+
+# # For testing
+# def print_linked_list(head):
+#     current = head
+#     while current:
+#         print(current.value, end=" -> " if current.next else "\n")
+#         current = current.next
+
+# def delete_tail(head):
+#    pass
+#    # input: Node instance -> head of linked list
+#    # output: Node instance -> head of linked list
+
+#    # Understand: Remove the last node in the linked list and return the list back
+
+#    # Plan:
+
+#    # Iterate through list while there's a curr.next.next value:
+#    # once loop is done, curr.next = none
+
+#    curr = head
+#    while curr.next.next:
+#       curr = curr.next
+   
+#    curr.next = None
+
+#    return head
+
+# butterfly = Node("Common Butterfly")
+# ladybug = Node("Ladybug")
+# beetle = Node("Scarab Beetle")
+# butterfly.next = ladybug
+# ladybug.next = beetle
+
+# # Input List: butterfly -> ladybug -> beetle
+# print_linked_list(delete_tail(butterfly))
+
+# 6
+
 class Node:
     def __init__(self, value, next=None):
         self.value = value
@@ -167,34 +210,37 @@ def print_linked_list(head):
         print(current.value, end=" -> " if current.next else "\n")
         current = current.next
 
-def delete_tail(head):
+def find_min(head):
    pass
-   # input: Node instance -> head of linked list
-   # output: Node instance -> head of linked list
 
-   # Understand: Remove the last node in the linked list and return the list back
+   # input: Node instance -> head of linked list
+   # output: int -> min mav in the linked list
+
+   # Understand: We want to find the min value in the linked list and return back to the user
 
    # Plan:
 
-   # Iterate through list while there's a curr.next.next value:
-   # once loop is done, curr.next = none
-
-   curr = head
-   while curr.next.next:
-      curr = curr.next
+   # initalize a min with the heads value
+   # iterate through the link list:
+      # check if curr node value is less than the min -> if so update min to that val
    
-   curr.next = None
+   # return min
 
-   return head
+   min = head.value
+   curr = head.next
 
-butterfly = Node("Common Butterfly")
-ladybug = Node("Ladybug")
-beetle = Node("Scarab Beetle")
-butterfly.next = ladybug
-ladybug.next = beetle
+   while curr:
+      if curr.value < min:
+         min = curr.value
+      curr = curr.next
 
-# Input List: butterfly -> ladybug -> beetle
-print_linked_list(delete_tail(butterfly))
+   return min
 
+head1 = Node(5, Node(6, Node(1, Node(8))))
+head2 = Node(8, Node(5, Node(6, Node(2))))
 
+# Linked List: 5 -> 6 -> 7 -> 8
+print(find_min(head1))
 
+# Linked List: 8 -> 5 -> 6 -> 7
+print(find_min(head2))
