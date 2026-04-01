28f2fae1d3695414a7b455c7a4944462917e3d00
Mario Nolasco Cortez <mariocortez7745@gmail.com>
2026-04-01 14:24:07 -0500

    Completed w6, s1, p4

--- week6/s1.py
+++ week6/s1.py
@@ -43,70 +43,112 @@ edge cases: N/A
 
 """ 2 """
 
+# class SongNode:
+#     def __init__(self, song, artist, next=None):
+#         self.song = song
+#         self.artist = artist
+#         self.next = next
+
+# # For testing
+# def print_linked_list(node):
+#     current = node
+#     while current:
+#         print((current.song, current.artist), end=" -> " if current.next else "")
+#         current = current.next
+#     print()
+
+
+# def get_artist_frequency(playlist):
+#    pass
+#    """
+#    # Understand: return a dic that maps each artist in list to its frequency 
+#    # { artistName: n}
+
+#    inputs: Linked list -> each node having a song and artist
+#    outputs: Dic -> artist with it's frequency
+#    constraints: N/A
+#    edge cases: Empty ll -> return {}
+
+#    # Match (any problems this reminds you of, any helpful patters to solve this e.g. two pointer technique, any data structures this reminds you of )
+
+#    # Plan (pseudocode)
+
+#    create a new dic
+#    iterate through the linked list
+#       get artist name, if artist not in dic -> add them to dic
+#       add 1 to dic[artist]
+
+#    return dic 
+   
+#    # Implement (python code)
+
+#    # Review (dry run of your code)
+
+#    # Evaluate (time and space complexity)
+#    """
+
+#    if not playlist:
+#        return {}
+
+#    dic = {}
+#    curr = playlist
+
+#    while curr:
+#       if curr.artist not in dic:
+#           dic[curr.artist] = 0
+#       dic[curr.artist] += 1
+#       curr = curr.next
+
+#    return dic
+
+# playlist = SongNode("Saturn", "SZA", 
+#                 SongNode("Who", "Jimin", 
+#                         SongNode("Espresso", "Sabrina Carpenter", 
+#                                 SongNode("Snooze", "SZA"))))
+
+# print(get_artist_frequency(playlist))
+
+""" 3 """
+
+# Understand: We want to check whether or not a linked list has a cycle at any point in the list. 
+
+# Input: SongNode instance -> head of a linked list
+# Output: Bool -> True if the playlist has a cycle in it, False otherwise
+
 class SongNode:
     def __init__(self, song, artist, next=None):
         self.song = song
         self.artist = artist
         self.next = next
 
-# For testing
-def print_linked_list(node):
-    current = node
-    while current:
-        print((current.song, current.artist), end=" -> " if current.next else "")
-        current = current.next
-    print()
+def on_repeat(playlist_head):
+    # plan
+    slow = playlist_head
+    fast = playlist_head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    
+    return False
+
+song1 = SongNode("GO!", "Common")
+song2 = SongNode("N95", "Kendrick Lamar")
+song3 = SongNode("WIN", "Jay Rock")
+song4 = SongNode("ATM", "J. Cole")
+song1.next = song2
+song2.next = song3
+song3.next = song4
+song4.next = song2
+
+print(on_repeat(song1))
 
 
-def get_artist_frequency(playlist):
-   pass
-   """
-   # Understand: return a dic that maps each artist in list to its frequency 
-   # { artistName: n}
 
-   inputs: Linked list -> each node having a song and artist
-   outputs: Dic -> artist with it's frequency
-   constraints: N/A
-   edge cases: Empty ll -> return {}
 
-   # Match (any problems this reminds you of, any helpful patters to solve this e.g. two pointer technique, any data structures this reminds you of )
 
-   # Plan (pseudocode)
-
-   create a new dic
-   iterate through the linked list
-      get artist name, if artist not in dic -> add them to dic
-      add 1 to dic[artist]
-
-   return dic 
-   
-   # Implement (python code)
-
-   # Review (dry run of your code)
-
-   # Evaluate (time and space complexity)
-   """
-
-   if not playlist:
-       return {}
-
-   dic = {}
-   curr = playlist
-
-   while curr:
-      if curr.artist not in dic:
-          dic[curr.artist] = 0
-      dic[curr.artist] += 1
-      curr = curr.next
-
-   return dic
-
-playlist = SongNode("Saturn", "SZA", 
-                SongNode("Who", "Jimin", 
-                        SongNode("Espresso", "Sabrina Carpenter", 
-                                SongNode("Snooze", "SZA"))))
-
-print(get_artist_frequency(playlist))
 
 
 
